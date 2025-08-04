98479ffdc5c929d933a0bfd438df7b3a66447c15
Michaeltse0426 <michaeltsegpt@gmail.com>
2025-08-04 08:37:19 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('nordic-trip-cache-v3').then(cache => {
+    caches.open('nordic-trip-cache-v4').then(cache => {
       return cache.addAll([
         './',
         './index.html',

d53a18f94a16704113d1306b62a3648b6984d69c
Michaeltse0426 <michaeltsegpt@gmail.com>
2025-08-04 11:38:44 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,7 @@
-
-const CACHE_NAME = 'nordic-trip-cache-v12-full';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './itinerary.json',
-  './refresh.mp3'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+self.addEventListener('install', e => {
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request).catch(() => new Response('Offline Mode')));
 });

ddc076ce8e020692e7579278f81b405650fa287a
Michaeltse0426 <michaeltsegpt@gmail.com>
2025-08-04 11:24:50 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,33 @@
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('nordic-trip-cache-v12').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './itinerary.json'
-      ]);
-    })
+const CACHE_NAME = 'nordic-trip-cache-v12-full';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './itinerary.json',
+  './refresh.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
